fbaf255bd2d146b01550cc04d99eded28d2ec540
vando ramos <vandorramos@gmail.com>
2025-11-05 15:42:45 -0300

    Added ability to delete a task

--- app/javascript/src/components/Tasks/Table/Row.jsx
+++ app/javascript/src/components/Tasks/Table/Row.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Tooltip } from "components/commons";
 import PropTypes from "prop-types";
 
-const Row = ({ data, showTask }) => (
+const Row = ({ data, showTask, destroyTask }) => (
   <tbody className="divide-y divide-gray-200 bg-white">
     {data.map((rowData) => (
       <tr key={rowData.id}>
@@ -17,6 +17,18 @@ const Row = ({ data, showTask }) => (
             Show
           </a>
         </td>
+        <td
+          className="cursor-pointer px-6 py-4 text-right
+            text-sm font-medium leading-5"
+        >
+          <a
+            className="text-red-500
+              hover:text-red-700"
+            onClick={() => destroyTask(rowData.slug)}
+          >
+            Delete
+          </a>
+        </td>
       </tr>
     ))}
   </tbody>
@@ -24,6 +36,7 @@ const Row = ({ data, showTask }) => (
 
 Row.propTypes = {
   data: PropTypes.array.isRequired,
+  destroyTask: PropTypes.func,
   showTask: PropTypes.func,
 };
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :tasks, only: %i[index show create update], param: :slug
+  resources :tasks, only: %i[index show create update destroy], param: :slug
 
   root "home#index"
   get "*path", to: "home#index", via: :all
